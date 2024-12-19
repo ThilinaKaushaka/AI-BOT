ac2ee86f2c553befabd8915d0bb7ad0f3b2b39a9
ThilinaKaushaka <thilinaw72@gmail.com>
2024-12-20 04:48:28 +0530

    first commit

---
+++ javaScript/script.js
@@ -0,0 +1,60 @@
+var L = true;
+search();
+
+function search() {
+    if ($('.text').val() != ""||L) {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const raw = JSON.stringify({
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": `${L==true?"Hello":$(".text").val()}`
+                        }
+                    ]
+                }
+            ]
+        });
+
+        addChat(true, null);
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key= YOUR API KEY ", requestOptions)
+            .then((response) => response.json())
+            .then((res) => {
+                addChat(false, res);
+            });
+    }
+}
+
+function addChat(Lc, res) {   
+    if (Lc) {
+        if (!L) {          
+            $('.c').append(
+                `<div class="me" data-aos="zoom-in">
+                    <p class="animate__animated animate__headShake" >${$('.text').val()}&nbsp;</p>
+                    
+                </div>
+                <br>
+                `  
+            );
+        }
+        $('.text').val("");
+    } else {
+        $('.c').append(
+            `<div class="bot" data-aos="zoom-in">
+                <p class="animate__animated animate__headShake">${res.candidates[0].content.parts[0].text}</p>               
+            </div>
+            <br>
+            `
+        );
+        L=false;
+    }   
+}
